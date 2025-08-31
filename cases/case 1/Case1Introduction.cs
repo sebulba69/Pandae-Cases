557b0cc98d81add051d07efb8eb747441a7655df
sebulba <72589301+sebulba69@users.noreply.github.com>
2025-08-30 20:02:13 -0700

    Initial pull

--- cases/case 1/Case1Introduction.cs
+++ cases/case 1/Case1Introduction.cs
@@ -11,38 +11,38 @@ using System.Threading.Tasks;
 
 public class Case1Introduction : Location
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        TextureButton buttonScene = GetNode<TextureButton>("%ButtonScene");
-        buttonScene.Connect("pressed", this, "BodyExamined");
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		TextureButton buttonScene = GetNode<TextureButton>("%ButtonScene");
+		buttonScene.Connect("pressed", this, "BodyExamined");
 
-        DialogBox = GetNode<DialogBox>("%DialogBox");
-        Characters = new Dictionary<string, CharacterNode>()
-        {
-            { G_Chars.Pandae, GetNode<CharacterNode>("%pandae") },
-            { G_Chars.Smirkfred, GetNode<CharacterNode>("%smirkfred") }
-        };
-        Music = GetNode<AudioStreamPlayer>("%AudioStreamPlayer");
-        SFX = GetNode<AudioStreamPlayer>("%SFXPlayer");
-        Background = GetNode<Background>("%Background");
+		DialogBox = GetNode<DialogBox>("%DialogBox");
+		Characters = new Dictionary<string, CharacterNode>()
+		{
+			{ G_Chars.Pandae, GetNode<CharacterNode>("%pandae") },
+			{ G_Chars.Smirkfred, GetNode<CharacterNode>("%smirkfred") }
+		};
+		Music = GetNode<AudioStreamPlayer>("%AudioStreamPlayer");
+		SFX = GetNode<AudioStreamPlayer>("%SFXPlayer");
+		Background = GetNode<Background>("%Background");
 
-        commands = Case1Dialog.Introduction;
+		commands = Case1Dialog.Introduction;
 
-        List<AnimationPlayer> players = new List<AnimationPlayer>()
-        {
-            GetNode<AnimationPlayer>("%AnimationPlayer"),
-            GetNode<AnimationPlayer>("%AnimationPlayer2"),
-            GetNode<AnimationPlayer>("%AnimationPlayer3"),
-        };
+		List<AnimationPlayer> players = new List<AnimationPlayer>()
+		{
+			GetNode<AnimationPlayer>("%AnimationPlayer"),
+			GetNode<AnimationPlayer>("%AnimationPlayer2"),
+			GetNode<AnimationPlayer>("%AnimationPlayer3"),
+		};
 
-        RegisterPlayers(players);
+		RegisterPlayers(players);
 
-        Run();
-    }
+		Run();
+	}
 
-    private void BodyExamined()
-    {
-        InvestigateSpot(G_Case1.C1_POI_Body);
-    }
+	private void BodyExamined()
+	{
+		InvestigateSpot(G_Case1.C1_POI_Body);
+	}
 }
